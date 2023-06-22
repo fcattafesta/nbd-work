e751ff86763c213d6075b35a1477a76ed836a638
fcattafesta <filippo.cattafesta99@gmail.com>
2023-06-22 16:04:37 +0200

    Final script

--- dyzpt.sh
+++ dyzpt.sh
@@ -7,7 +7,7 @@ PYTHON_SCRIPT="single_file.py"
 DIRECTORY="/gpfs/ddn/srm/cms//store/mc/RunIISummer20UL18NanoAODv9/DYJetsToLL_LHEFilterPtZ-0To50_MatchEWPDG20_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/"
 
 # Loop through the files in the directory and its subdirectories
-find $DIRECTORY -type f | while read FILE; do
+find "$DIRECTORY" -type f -print0 | while read -d $'\0' FILE; do
     echo "Running script for $FILE"
     python $PYTHON_SCRIPT "$FILE"
     if [ $? -ne 0 ]; then
